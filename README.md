8cc710ae5d921784d382f78e7cd904d43dc8a36f
Joao Miguel Stroher <joao.stroher@gmail.com>
2020-06-04 13:04:07 -0300

    initial project

---
+++ README.md
@@ -0,0 +1,7 @@
+# Insomnia Gist Sync
+
+This is a plugin for [Insomnia](https://insomnia.rest) that allows users sync workspaces with gist of GitHub.
+
+## Installation
+
+Install the `insomnia-plugin-gist-sync` plugin from Preferences > Plugins.
---
+++ src/index.js
@@ -0,0 +1,156 @@
+const fs = require('fs');
+const axios = require('axios');
+
+function createGitHubApi(token) {
+  return axios.create({
+    baseURL: 'https://api.github.com/',
+    timeout: 5000,
+    headers: { Authorization: `Bearer ${token}` },
+  });
+}
+
+async function promptApiKey(context, forceReprompt) {
+  const oldApiKey = await context.store.getItem('gist-sync:api-key');
+  if (!oldApiKey || forceReprompt) {
+    try {
+      const apiKey = await context.app.prompt(
+        'Gist Sync - Configuration - API Key',
+        {
+          label: 'Put your gist api key here:',
+          defaultValue: oldApiKey,
+          submitName: 'Save',
+          cancelable: true,
+        },
+      );
+      const sanitizedApiKey = apiKey.trim();
+      if (sanitizedApiKey === '') {
+        context.store.removeItem('gist-sync:api-key');
+        return null;
+      }
+      await context.store.setItem('gist-sync:api-key', sanitizedApiKey);
+      return sanitizedApiKey;
+    } catch {}
+  }
+  return oldApiKey;
+}
+
+async function promptGistKey(context, forceReprompt) {
+  const oldGistKey = await context.store.getItem('gist-sync:gist-key');
+  if (!oldGistKey || forceReprompt) {
+    try {
+      const gistKey = await context.app.prompt(
+        'Gist Sync - Configuration - Gist Key',
+        {
+          label:
+            'Put your gist key here (if not present, a new gist will be created):',
+          defaultValue: oldGistKey,
+          submitName: 'Save',
+          cancelable: true,
+        },
+      );
+      const sanitizedGistKey = gistKey.trim();
+      if (sanitizedGistKey === '') {
+        context.store.removeItem('gist-sync:gist-key');
+        return null;
+      }
+      await context.store.setItem('gist-sync:gist-key', sanitizedGistKey);
+      return sanitizedGistKey;
+    } catch {}
+  }
+  return oldGistKey;
+}
+
+module.exports.workspaceActions = [
+  {
+    label: 'Gist Sync - Send',
+    icon: 'fa-upload',
+    action: async (context, models) => {
+      const apiKey = await promptApiKey(context, false);
+      if (!apiKey) {
+        await context.app.alert(
+          'Configuration error',
+          'The Gist API Key must be configured',
+        );
+        return;
+      }
+      const gistKey = await promptGistKey(context, false);
+      if (!gistKey) {
+        if (!confirm('The Gist Key not informated, will be create a new gist?'))
+          return;
+      }
+      const data = await context.data.export.insomnia({
+        includePrivate: false,
+        format: 'json',
+      });
+      const content = JSON.stringify(JSON.parse(data), null, 2);
+      if (!gistKey) {
+        const response = await createGitHubApi(apiKey).post(
+          'https://api.github.com/gists',
+          {
+            files: {
+              'insomnia_data.json': {
+                content,
+              },
+            },
+            description: 'Insomnia Sync Data',
+            public: false,
+          },
+        );
+        await context.store.setItem('gist-sync:gist-key', response.data.id);
+      } else {
+        await createGitHubApi(apiKey).patch(
+          `https://api.github.com/gists/${gistKey}`,
+          {
+            files: {
+              'insomnia_data.json': {
+                content,
+              },
+            },
+          },
+        );
+      }
+    },
+  },
+  {
+    label: 'Gist Sync - Receive',
+    icon: 'fa-download',
+    action: async (context, models) => {
+      const apiKey = await promptApiKey(context, false);
+      console.log('apiKey', apiKey);
+      if (!apiKey) {
+        console.log('apiKey', apiKey);
+        await context.app.alert(
+          'Configuration error',
+          'The Gist API Key must be configured',
+        );
+        return;
+      }
+      const gistKey = await promptGistKey(context, false);
+      if (!gistKey) {
+        await context.app.alert(
+          'Configuration error',
+          'The Gist Key must be configured',
+        );
+        return;
+      }
+      const response = await createGitHubApi(apiKey).get(
+        `https://api.github.com/gists/${gistKey}`,
+      );
+      const file = response.data.files['insomnia_data.json'];
+      if (!file.truncated) await context.data.import.raw(file.content);
+      else {
+        const rawResponse = await axios.get(file.raw_url);
+        const content = JSON.stringify(rawResponse.data);
+        await context.data.import.raw(content);
+      }
+    },
+  },
+  {
+    label: 'Gist Sync - Configure',
+    icon: 'fa-cogs',
+    action: async (context, models) => {
+      await promptApiKey(context, true);
+      await promptGistKey(context, true);
+    },
+  },
+];
